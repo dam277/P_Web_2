8eccacb93f60bb05677a2d39c7eac625ba14b410
Thomas Rey <92440325+ThomasRey1@users.noreply.github.com>
2022-03-30 14:34:43 +0200

    Update site.php

--- site.php
+++ site.php
@@ -1,7 +1,7 @@
 <?php
 header("Location: 02-CodeSource/Site/index.php");
 ?>
-~ $ curl -i -X POST -d '_InField1=RESTRIC&_InField2=code&_InField3=13D2' https://telebears.berkeley.edu/enrollment-osoc/osc
+~ $ curl -i -X POST -d '_InField1=RESTRIC&_InField2=code&_InField3=13D2' https://p-web-2.herokuapp.com/
 HTTP/1.1 403 Forbidden
 Date: Tue, 24 Sep 2013 03:07:17 GMT
 Content-Length: 310
