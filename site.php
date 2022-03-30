c84ab6a9731128498e22260adce1c8eec58fea1c
Thomas Rey <92440325+ThomasRey1@users.noreply.github.com>
2022-03-30 14:34:20 +0200

    Update site.php

--- site.php
+++ site.php
@@ -1,3 +1,9 @@
 <?php
 header("Location: 02-CodeSource/Site/index.php");
 ?>
+~ $ curl -i -X POST -d '_InField1=RESTRIC&_InField2=code&_InField3=13D2' https://telebears.berkeley.edu/enrollment-osoc/osc
+HTTP/1.1 403 Forbidden
+Date: Tue, 24 Sep 2013 03:07:17 GMT
+Content-Length: 310
+Connection: close
+Content-Type: text/html; charset=iso-8859-1
