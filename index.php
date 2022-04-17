fe66c86b1ed0aa0ee5171ea1606438f116b4c761
Damloup <damis.loup@gmail.com>
2022-04-17 23:11:01 +0200

    Terminé : Page - ajout d'ouvrage

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-header("Location: 02-CodeSource/Site/src/php/views/home.php");
+    //header("Location: 02-CodeSource/Site/src/php/views/home.php");
+    header("Location: 02-CodeSource/Site/src/php/views/addBook.php");
+    //header("Location: 02-CodeSource\Tests\displayTextOnImage\DisplayTextOnImageTest.php");
+    //header("Location: 02-CodeSource\Tests\defilementImages\index.php");
 ?>
