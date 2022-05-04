c6a682d2e2c200a39998ca5570908f3f25bb1937
Thomas Rey <thorey@etmlnet.local>
2022-05-04 16:30:48 +0200

    Création de la page de connexion

---
+++ 02-CodeSource/Site/src/php/views/connexion.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+
+<body class="duration-[0.5s] dark:bg-gray-700 dark:text-gray-400 bg-[url('../../../resources/background.png')]">
+    <header>
+        <?php
+        //include("../../include/header.php");
+        ?>
+    </header>
+    <main class="grid place-items-center">
+        <h1 class="text-4xl md:text-6xl lg:text-8xl xl:text-8xl text-white text-center bg-[#0A183C] w-full py-[200px]">CONNEXION</h1>
+        <div class="w-[500px] h-[600px] mt-[100px] bg-gradient-to-b from-[#05687E] to-[#C1E0E7] rounded-[25px] text-center">
+            <p class="text-6xl">Se connecter</p>
+            <div class="flex mt-[10px]">
+                <div class="w-[25%]"></div><div class="w-[50%] border-b-[1px] border-black"></div><div class="w-[25%]"></div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <?php
+        //include("../../include/footer.php");
+        ?>
+    </footer>
+</body>
+
+</html>
