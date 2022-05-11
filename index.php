490c995c0d129d9983bd37be28f35199c7f7b6c0
Damloup <60733960+dam277@users.noreply.github.com>
2022-05-11 10:45:33 +0200

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Location: 02-CodeSource/Site/src/php/views/home.php");
-//header("Location: 02-CodeSource\Tests\displayTextOnImage\DisplayTextOnImageTest.php");
-//header("Location: 02-CodeSource\Tests\defilementImages\index.php");
-?>
